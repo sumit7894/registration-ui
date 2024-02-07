8b3f13719b0fa08d72d98e9974707bd04c566acc
Sumit Maurya <smithdashdash@gmail.com>
2024-02-07 15:41:26 +0530

    added toggle for sign in page

--- src/Components/Auth/Auth.js
+++ src/Components/Auth/Auth.js
@@ -1,18 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './auth.module.css'
 import Google from '../../assets/Icons/Google.png';
 import Facebook from '../../assets/Icons/Facebook.png';
 const Auth = () => {
+    const [authMode,setAuthMode] = useState(true);
   return (
     <div className={styles.main__auth}>
         <div className={styles.form__container}>
             <h1  className={styles.form__heading}>
-                Create an Account
+                {authMode ? "Create an Account":"Sign In"}
             </h1>
             <h2 className={styles.form__subheading}>
-                Have an Account?
-                <span className={styles.toggle__button}>
-                    Sign In
+                {authMode ? "Have an Account?":"New to Our Product?"}
+                <span className={styles.toggle__button} onClick={()=>setAuthMode((prev)=>!prev)}>
+                    {authMode ? "Sign In":"Create an Account"}
                 </span>
             </h2>
         <div className={styles.main__form__container}>
@@ -29,18 +30,26 @@ const Auth = () => {
                 placeholder='Create Password'
                 />
                 </div>
+                {authMode ? (<></>):(
+                    <div className={styles.checkbox__container}>
+                        <input type='checkbox'/>
+                        <span>Keep me signed in</span>
+                    </div>
+                )}
                 <button type='submit' className={styles.submit__button}>
-                    Create Account
+                    {authMode ? "Create Account":"Sign In"}
                 </button>
             </form>
         </div> 
         <label className={styles.message__footer}>
-            By creating account, you agree to our
+        {authMode ? "By creating account, you agree to our":<></>}
         </label>
-        <span className={styles.terms}>Terms of Service</span>
+        <span className={styles.terms}>
+            {authMode ? "Terms of Service":"Forgot your password?"}
+        </span>
         <div className={styles.horizontal__line}></div>
         <span className={styles.footer__heading}>
-            Or create an account using:
+        {authMode ? "Or create an account using:":"Or sign in using:"}
         </span>
         <div className={styles.footer__cards}>
             <img src={Google} alt='google'/>
